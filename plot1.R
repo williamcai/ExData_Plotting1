fd1dce0ebe707aa42f4563c11908f0a46b7edca5
williamcai <caiwg2012@yeah.net>
2014-06-15 00:15:31 +0800

    Plot1 commit

--- plot1.R
+++ plot1.R
@@ -1,11 +1,21 @@
-data <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?",
-                   stringsAsFactors = FALSE)
+# coursera: Exploratory Data Analysis
+# course project 
+# Plot 1
+
+# load data from file:household_power_consumption.txt
+data <- read.table("household_power_consumption.txt",
+					header=TRUE,
+          sep=";",
+					na.strings = "?",
+					stringsAsFactors = FALSE)
+# convert date type
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
+# subsettig data
 d <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02",]
 
-x <- d$Global_active_power
-hist(x,
+# plot
+hist(d$Global_active_power,
      main="Global Active Power",
      xlab = "Global Active Power (kilowatts)",
      col="red")
